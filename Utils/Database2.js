323e935ef9399dff473ba034f8b1e30376bc17a2
Tanya933 <tanya.rajpal0400@gmail.com>
2021-04-20 11:44:54 +0530

    initialization

--- Utils/Database2.js
+++ Utils/Database2.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var pool  = mysql.createPool({
     host     : 'localhost',
     user     : 'root',
-    password : 'aaditya123',
+    password : 'tanyarajpal123',
     database : 'client'
 });
 
